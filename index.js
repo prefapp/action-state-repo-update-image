b24a2551b139f6715e0974098bfc9392ef7964ea
AlbertoFemenias <alberto.femenias@udc.es>
2021-06-16 13:38:37 +0200

    new tests and classes

--- index.js
+++ index.js
@@ -1,40 +1,41 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const yaml = require('js-yaml');
-const fs   = require('fs');
+const ghUtils = require('./ghUtils')
+//const autoMergeFromYaml = require('./autoMergeFromYaml');
 
 // most @actions toolkit packages have async methods
+
 async function run() {
   try {
-    /*const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    /*core.setOutput('time', new Date().toTimeString());*/
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());*/
-    console.log("READING config.yaml");
-    // Get document, or throw exception on error
-    try {
-      const doc = yaml.load(fs.readFileSync('./config.yaml', 'utf8'));
-      console.log(doc);
-    } catch (e) {
-      console.log(e);
-    }
-
-    const myToken = core.getInput('secret_token');
-    const targetBranch = core.getInput('target_branch');
-    const octokit = github.getOctokit(myToken)
+    const inputs = {
+      secret_token: core.getInput('secret_token'),
+      target_branch: core.getInput('target_branch')
+    };
+    
+    const octokit = github.getOctokit(inputs.secret_token)
     const context = github.context;
 
-    const repoName = context.payload.repository.full_name;
-    const repoDefaultBranch = context.payload.repository.default_branch;
-    const repoOwner = context.payload.repository.owner.login;
+    let ghClient = new ghUtils(context, octokit);
 
+    const prNumber = ghClient.createPr(inputs.target_branch, "NEW PR FROM GH ACTIONS")
+    core.info('Created PR number: ' + prNumber);
+    ghClient.prAddReviewers(prNumber, ["AlbertoFemenias"]);
+    core.info('Added reviewers: ' + ["AlbertoFemenias"]);
+    //autoMerge = autoMergeFromYaml('./config.yaml',  aplication, enviroment);
+    ghClient.mergePr(prNumber);
+    core.info('Successfully merged PR number: ' + prNumber);
+
+    //const repoName = context.payload.repository.full_name;
+    //const repoDefaultBranch = context.payload.repository.default_branch;
+    //const repoOwner = context.payload.repository.owner.login;
+
+
+    /*
     const prInputs = {
       ...context.repo,
-      head: targetBranch,
+      head: inputs.target_branch,
       base: repoDefaultBranch,
       title: 'PR title'
     }
@@ -44,22 +45,23 @@ async function run() {
     const newPr = await octokit.rest.pulls.create(prInputs);
     //console.log(newPr);
 
-    prNumber = newPr.data.number;
+    const prNumber = newPr.data.number;*/
 
-    const prReviewers = await octokit.rest.pulls.requestReviewers({
+    
+    /*const prReviewers = await octokit.rest.pulls.requestReviewers({
       ...context.repo,
       pull_number: prNumber,
       reviewers: ["AlbertoFemenias"]
     });
 
-    //console.log(prReviewers);
-
+    //console.log(prReviewers);*/
+    /*
     const autoMerge = await octokit.rest.pulls.merge({
       ...context.repo,
       pull_number: prNumber
     });
 
-    console.log(autoMerge); 
+    console.log(autoMerge); */
 
 
   } catch (error) {
--- index.test.js
+++ index.test.js
@@ -1,8 +1,10 @@
 const wait = require('./wait');
+const autoMergeFromYaml = require('./autoMergeFromYaml');
 const process = require('process');
 const cp = require('child_process');
 const path = require('path');
-
+var fs = require('fs');
+/*
 test('throws invalid number', async () => {
   await expect(wait('foo')).rejects.toThrow('milliseconds not a number');
 });
@@ -14,12 +16,47 @@ test('wait 500 ms', async () => {
   var delta = Math.abs(end - start);
   expect(delta).toBeGreaterThanOrEqual(500);
 });
+*/
 
 // shows how the runner will run a javascript action with env / stdout protocol
-/* commented to prevent FAILURE
+/*
 test('test runs', () => {
   process.env['INPUT_MILLISECONDS'] = 500;
   const ip = path.join(__dirname, 'index.js');
   console.log(cp.execSync(`node ${ip}`, {env: process.env}).toString());
 })
 */
+
+
+// Test read config yaml autoMergeFromYaml
+test('autoMergeFromYaml yaml not found', () => {
+  expect(() => {
+    autoMergeFromYaml('./foo/bar.yaml', "app1", "des")
+  }).toThrow('Error trying to read configFile: ');
+});
+
+test('autoMergeFromYaml invalid enviroment', () => {
+  expect(() => {
+    autoMergeFromYaml('./fixtures/config.yaml', "app2", "dev")
+  }).toThrow("Enviroment dev not in [des, pre, pro]");
+});
+
+test('autoMergeFromYaml invalid enviroment', () => {
+  expect(() => {
+    autoMergeFromYaml('./fixtures/config.yaml', "app3", "des")
+  }).toThrow("Application not found: app3");
+});
+
+test('autoMergeFromYaml correct execution', () => {
+  //console.log(fs.readdirSync('./fixtures'));
+  const autoMergeDes = autoMergeFromYaml('./fixtures/config.yaml', "app1", "des");
+  expect(autoMergeDes).toBe(true);
+
+  const autoMergePre = autoMergeFromYaml('./fixtures/config.yaml', "app2", "pre");
+  expect(autoMergePre).toBe(true);
+
+  const autoMergePro = autoMergeFromYaml('./fixtures/config.yaml', "app1", "pro");
+  expect(autoMergePro).toBe(false);
+});
+
+
