08aa68f3c6661a657031c637d2082ea6d19e3258
AlbertoFemenias <alberto.femenias@udc.es>
2021-06-16 10:57:56 +0200

    reading yamlks

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const yaml = require('js-yaml');
+const fs   = require('fs');
 
 // most @actions toolkit packages have async methods
 async function run() {
@@ -12,6 +14,14 @@ async function run() {
     core.info((new Date()).toTimeString());
 
     core.setOutput('time', new Date().toTimeString());*/
+    console.log("READING config.yaml");
+    // Get document, or throw exception on error
+    try {
+      const doc = yaml.load(fs.readFileSync('./config.yaml', 'utf8'));
+      console.log(doc);
+    } catch (e) {
+      console.log(e);
+    }
 
     const myToken = core.getInput('secret_token');
     const targetBranch = core.getInput('target_branch');
