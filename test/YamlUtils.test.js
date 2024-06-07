e0dd44f9eb06ab996e3e44a4b28011bb3f3909d9
Jose Alvarez <47305925+jalvarezit@users.noreply.github.com>
2024-06-07 18:51:44 +0200

    Fix tests

--- test/YamlUtils.test.js
+++ test/YamlUtils.test.js
@@ -1,5 +1,5 @@
 const yamlUtils = require('../utils/YamlUtils.js');
-
+const basePath = "";
 
 test('determineAutoMerge file not found', () => {
   expect(() => {
@@ -37,21 +37,21 @@ test('loadYaml failure', () => {
 
 test('modifyImage correct execution', () => {
   // you need 2 changes of each to make sure it changes
-  yamlUtils.modifyImage("fixtures/tenant1", "release1", "dev", "proxy", "foo/proxy:bar");
+  yamlUtils.modifyImage(basePath, "fixtures/tenant1", "release1", "dev", "proxy", "foo/proxy:bar");
   const modImages11dev = yamlUtils.loadYaml('./fixtures/tenant1/release1/dev/images.yaml');
   expect(modImages11dev["proxy"]["image"]).toBe("foo/proxy:bar");
 
-  const oldValue1 = yamlUtils.modifyImage("fixtures/tenant1", "release1", "dev", "proxy", "foo/proxy:dev");
+  const oldValue1 = yamlUtils.modifyImage(basePath, "fixtures/tenant1", "release1", "dev", "proxy", "foo/proxy:dev");
   expect(oldValue1).toBe("foo/proxy:bar");
 
   const images11devRestored = yamlUtils.loadYaml('./fixtures/tenant1/release1/dev/images.yaml');
   expect(images11devRestored["proxy"]["image"]).toBe("foo/proxy:dev");
 
-  yamlUtils.modifyImage("fixtures/tenant2", "releaseB", "pro", "app-server", "foo/common:pro");
+  yamlUtils.modifyImage(basePath, "fixtures/tenant2", "releaseB", "pro", "app-server", "foo/common:pro");
   const modimages2Bpro = yamlUtils.loadYaml('./fixtures/tenant2/releaseB/pro/images.yaml');
   expect(modimages2Bpro["app-server"]["image"]).toBe("foo/common:pro");
 
-  const oldValue2 = yamlUtils.modifyImage("fixtures/tenant2", "releaseB", "pro", "app-server", "foo/common:bar" );
+  const oldValue2 = yamlUtils.modifyImage(basePath, "fixtures/tenant2", "releaseB", "pro", "app-server", "foo/common:bar" );
   expect(oldValue2).toBe("foo/common:pro");
   const images2BproRestore = yamlUtils.loadYaml('./fixtures/tenant2/releaseB/pro/images.yaml');
   expect(images2BproRestore["app-server"]["image"]).toBe("foo/common:bar");
@@ -61,11 +61,11 @@ test('modifyImage correct execution', () => {
 
 test('modifyImage failure', () => {
   expect(() => {
-    yamlUtils.modifyImage("fixtures/tenant2", "releaseB", "production", "app-server", "foo/common:bar");
+    yamlUtils.modifyImage(basePath, "fixtures/tenant2", "releaseB", "production", "app-server", "foo/common:bar");
   }).toThrow("Error trying to read yaml file: ");
 
   expect(() => {
-    yamlUtils.modifyImage("fixtures/tenant2", "releaseB", "pro", "inexistent_service", "foo/common:bar");
+    yamlUtils.modifyImage(basePath, "fixtures/tenant2", "releaseB", "pro", "inexistent_service", "foo/common:bar");
   }).toThrow("Error: no service inexistent_service found in file ./fixtures/tenant2/releaseB/pro/images.yaml");
 
   
